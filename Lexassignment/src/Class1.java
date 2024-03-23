f513976fdc0999a85f9da80d2096a31c84309ba9
mvkalyani <mveerakalyani@gmail.com>
2024-03-23 12:00:28 +0530

    2 commit

--- Lexassignment/src/Class1.java
+++ Lexassignment/src/Class1.java
@@ -4,7 +4,9 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String  nameString ="class1";
+		String  nameBranch ="Branch1";
 		System.out.println("First:"+nameString);
+		System.out.println("FirstBranch:"+nameBranch);
 	}
 
 }
